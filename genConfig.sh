fdfedd5791e2378f9f226b70d9527ce75811f22d
Vincent Fournier <vince@ntfournier.com>
2017-04-03 14:32:16 -0400

    Improve genConfig script

--- genConfig.sh
+++ genConfig.sh
@@ -1,15 +1,24 @@
-#/bin/sh
+#!/bin/sh
+BACKUP_LOCATION="$PWD/cfg/"
 
-CFG_LOCATION="$PWD/cfg/yolo-sort.cfg"
-NAMES_LOCATION="$PWD/cfg/obj.names"
-DATA_LOCATION="$PWD/cfg/obj.data"
-TRAIN_LOCATION="$PWD/cfg/train.txt"
-TEST_LOCATION="$PWD/cfg/test.txt"
-NEURAL_LOCATION="$PWD/cfg/darknet19_448.conv.23"
+DARKNET_LOCATION="/usr/src/darknet"
+CFG_LOCATION="$DARKNET_LOCATION/yolo-sort.cfg"
 
+DATA_LOCATION="$DARKNET_LOCATION/voc-sort.data"
+TRAIN_LOCATION="$DARKNET_LOCATION/data/train.txt"
+TEST_LOCATION="$DARKNET_LOCATION/data/test.txt"
+NAMES_LOCATION="$DARKNET_LOCATION/data/sort.names"
 
-echo "Calculating number of classes and filters ..."
-CLASSES=`ls -l ./data/labels/ | grep -c ^d`
+NEURAL_LOCATION="$DARKNET_LOCATION/darknet19_448.conv.23"
+
+echo "################################################################################"
+echo "# Creating configurations for Yolo                                             #"
+echo "################################################################################"
+echo ""
+
+echo "# Calculating number of classes and filters ..."
+echo "################################################################################"
+CLASSES=`find ./data/classes/ -type f | wc -l`
 FILTERS="$((($CLASSES+5)*5))"
 
 echo "Classes: $CLASSES"
@@ -17,7 +26,7 @@ echo "Filters: $FILTERS"
 echo ""
 
 
-echo "Generating $CFG_LOCATION ..."
+echo "Replacing template in $CFG_LOCATION ..."
 cp -f ./templates/yolo-sort.cfg $CFG_LOCATION
 sed -i.bak "s/<classes>/$CLASSES/" $CFG_LOCATION
 sed -i.bak "s/<filters>/$FILTERS/" $CFG_LOCATION
@@ -25,25 +34,37 @@ echo "Done."
 echo ""
 
 
-echo "Generating train and test set ..."
+echo "# Generating train and test set ..."
+echo "################################################################################"
 # This is a cheap way to split data
 find $PWD/data/images | grep --include=*.{jpg,JPG} [1-9]\\. > $TRAIN_LOCATION
 find $PWD/data/images | grep --include=*.{jpg,JPG} [0]\\. > $TEST_LOCATION
+
+NB_TRAIN=`cat $TRAIN_LOCATION | wc -l`
+NB_TEST=`cat $TEST_LOCATION | wc -l`
+
+echo "Number of picture for training: $NB_TRAIN"
+echo "Number of picture for testing: $NB_TEST"
+echo ""
+
 echo "Done."
 echo ""
 
 
-echo "Generating obj.names ..."
+echo "# Generating obj.names (possible classes) ..."
+echo "################################################################################"
 ls -l ./data/classes/ | tail --line=+2  > cfg/label.bak
 awk '{ print $11}' cfg/label.bak | cut -f1 -d'.' > $NAMES_LOCATION
 
 echo "Classes found:"
 cat $NAMES_LOCATION
+echo ""
 echo "Done."
 echo ""
 
 
-echo "Generating obj.data ..."
+echo "# Generating obj.data ..."
+echo "################################################################################"
 cp -f ./templates/obj.data $DATA_LOCATION
 sed -i.bak "s/<classes>/$CLASSES/" $DATA_LOCATION
 sed -i.bak "s,<train_location>,$TRAIN_LOCATION," $DATA_LOCATION
@@ -55,27 +76,40 @@ echo ""
 rm cfg/*.bak
 
 
-echo "Backup older Darknet configuration ..."
-mv -f /usr/src/darknet/yolo-sort.cfg /usr/src/darknet/yolo-sort.cfg-old
-
-
-echo "Copying configs to Darknet installation ..."
-cp -f $CFG_LOCATION /usr/src/darknet/
+echo "# Copying configs to training folder ..."
+echo "################################################################################"
+cp -f $CFG_LOCATION $BACKUP_LOCATION
+cp -f $DATA_LOCATION $BACKUP_LOCATION
+cp -f $NAMES_LOCATION $BACKUP_LOCATION
 echo "Done."
+echo ""
+echo " NOTES"
+echo "  - Files in $BACKUP_LOCATION are copies modifying them will not change what Yolo will use!"
+echo "  - Path are suppose to be corresponding on how you will run Darknet (e.g. /usr/src/darknet/ if run within Docker)"
 echo ""
 
 
-echo "Downloading pre-trained network ..."
+echo "# Downloading pre-trained network ..."
+echo "################################################################################"
 if [ ! -s $NEURAL_LOCATION ]; then
+    echo "Pre-trained network not found. Downloading"
     wget -O $NEURAL_LOCATION http://pjreddie.com/media/files/darknet19_448.conv.23
 else
     echo "Pre-trained network already downloaded."
 fi
 echo "Done."
 echo ""
+echo ""
 
 
-echo "Configs files generated you can now run this in the Darknet folder"
-echo "cd ../../darknet && ./darknet detector train $DATA_LOCATION $CFG_LOCATION $PWD"
-echo "Or test the detector on some data"
-echo "cd ../../darknet && ./darknet detect yolo-sort.cfg backup/yolo-sort_4000.weights /usr/src/server/training-data/data/images/cup_paper/IMG_1940.JPG"
+echo "################################################################################"
+echo "# Configs are generated"
+echo "################################################################################"
+echo ""
+echo "You can now run this in the Darknet folder"
+echo "    cd $DARKNET_LOCATION"
+echo "    ./darknet detector train $DATA_LOCATION $CFG_LOCATION $PWD"
+echo ""
+echo "Or test the detector on some data (if you've already train)"
+echo "cd ../../darknet && ./darknet detect yolo-sort.cfg <weights> <image_location>"
+echo ""
